0c49f2bc75ba4751d05d7b0c39733c3fe8f9b834
Henry Lee <nynxzx@gmail.com>
2024-06-25 15:46:22 +1000

    narbar update - add link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,13 +4,16 @@ import ThemeToggle from "@/components/theme-toggle";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 import { Button } from "./ui/button";
 import { Book, Bot, Code2, Settings2, SquareTerminal } from "lucide-react";
+import Link from "next/link";
 
 const NavBar = () => {
   return (
     <Card className="rounded-lg">
       <nav className="flex flex-row items-center justify-between">
         <ul className="flex flex-row px-2">
-          <li className="px-2 font-bold underline">Henry Lee</li>
+          <li className="px-2 font-bold underline">
+            <Link href="/">Henry Lee</Link>
+          </li>
         </ul>
         <div className="m-w-0 flex flex-row items-center gap-4">
           <h2>Create Post</h2>
