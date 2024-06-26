3dd109776f3d41ff1ec38656a178111d24e448c5
Henry Lee <nynxzx@gmail.com>
2024-06-26 16:34:39 +1000

    post page styling changes

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -8,6 +8,7 @@ import rehypeRaw from "rehype-raw";
 
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const PostPage = async ({ params }: { params: { id: string } }) => {
   let post = await getPost(params.id);
@@ -17,38 +18,40 @@ const PostPage = async ({ params }: { params: { id: string } }) => {
 
   return (
     <div className="m-2 flex flex-grow min-w-max justify-items-center">
-      <Card className="flex flex-grow flex-col justify-center items-center ">
-        <Card className="h-full m-8 p-8 w-2/3">
-          <h1 className="scroll-m-20 pb-4 text-4xl font-extrabold tracking-tight lg:text-5xl">
-            {post?.title}
-          </h1>
-          <Markdown
-            remarkPlugins={[RemarkGfm]}
-            rehypePlugins={[rehypeRaw]}
-            components={{
-              code({ node, inline, className, children, ...props }: any) {
-                const match = /language-(\w+)/.exec(className || "");
+      <Card className="flex flex-grow flex-col justify-items-center">
+        <ScrollArea className="flex flex-grow h-0 ">
+          <Card className="h-full m-8 p-8 w-2/3">
+            <h1 className="pb-4 text-4xl font-extrabold tracking-tight lg:text-5xl">
+              {post?.title}
+            </h1>
+            <Markdown
+              remarkPlugins={[RemarkGfm]}
+              rehypePlugins={[rehypeRaw]}
+              components={{
+                code({ node, inline, className, children, ...props }: any) {
+                  const match = /language-(\w+)/.exec(className || "");
 
-                return !inline && match ? (
-                  <SyntaxHighlighter
-                    style={oneDark}
-                    PreTag="div"
-                    language={match[1]}
-                    {...props}
-                  >
-                    {String(children).replace(/\n$/, "")}
-                  </SyntaxHighlighter>
-                ) : (
-                  <code className={className} {...props}>
-                    {children}
-                  </code>
-                );
-              },
-            }}
-          >
-            {post?.content}
-          </Markdown>
-        </Card>
+                  return !inline && match ? (
+                    <SyntaxHighlighter
+                      style={oneDark}
+                      PreTag="div"
+                      language={match[1]}
+                      {...props}
+                    >
+                      {String(children).replace(/\n$/, "")}
+                    </SyntaxHighlighter>
+                  ) : (
+                    <code className={className} {...props}>
+                      {children}
+                    </code>
+                  );
+                },
+              }}
+            >
+              {post?.content}
+            </Markdown>
+          </Card>
+        </ScrollArea>
       </Card>
     </div>
   );
